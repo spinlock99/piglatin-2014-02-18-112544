928f94adda406ca3eecc194009d4b5436a605aea
Andrew Dixon <andrew.dixon@twitch.tv>
2014-02-19 11:52:19 -0800

    kata complete

--- lib/piglatin.rb
+++ lib/piglatin.rb
@@ -1,2 +1,26 @@
-class Piglatin
+$vowels = 'aeiou'
+
+class PigLatin
+  attr_accessor :phrase
+
+  def phrase=(string)
+    @phrase = string
+  end
+
+  alias :initialize :phrase=
+
+  def translate
+    return(nil) if @phrase.empty?
+    return(@phrase.gsub(/(\w+)/) { |word| translate_word(word) })
+  end
+
+  private
+
+  def translate_word(word)
+    word += word.slice!(/^[^#$vowels]+/i) || ''
+    word.gsub!(/(y)$/i, '\1n')
+    word.gsub!(/([#$vowels])$/i, '\1y')
+    word.capitalize! if word.downcase!
+    return(word + 'ay')
+  end
 end
--- spec/piglatin_spec.rb
+++ spec/piglatin_spec.rb
@@ -1,12 +1,71 @@
 require 'spec_helper'
 require 'piglatin'
 
-describe Piglatin do
-  describe "new" do
+describe PigLatin do
+  describe "#new" do
+    subject { PigLatin.new('hello world') }
     it "should instantiate" do
-      lambda {
-        Piglatin.new
-      }.should_not raise_exception
+      expect { subject }.to_not raise_exception
+    end
+  end
+
+  describe ".translate" do
+    let(:pig_latin) { PigLatin.new(phrase) }
+    subject { pig_latin.translate }
+
+    context 'empty phrase' do
+      let(:phrase) { "" }
+      it { should eq(nil) }
+    end
+
+    context 'words that start with vowels' do
+      let(:phrase) { 'ask' }
+      it { should eq('askay') }
+
+      context 'and end with a vowel' do
+        let(:phrase) { 'apple' }
+        it { should eq('appleyay') }
+      end
+
+      context 'and end in y' do
+        let(:phrase) { 'any' }
+        it { should eq('anynay') }
+      end
+    end
+
+    context 'words that start with consonants' do
+      let(:phrase) { 'hello' }
+      it { should eq('ellohay') }
+    end
+    context 'multiple consonants' do
+      let(:phrase) { 'known' }
+      it { should eq('ownknay') }
+    end
+
+    context 'multiple words' do
+      let(:phrase) { 'hello world' }
+      it { should eq('ellohay orldway') }
+    end
+
+    context 'hyphenated words' do
+      let(:phrase) { 'well-being' }
+      it { should eq('ellway-eingbay') }
+    end
+
+    context 'capital letters' do
+      context 'Bill' do
+        let(:phrase) { 'Bill' }
+        it { should eq('Illbay') }
+      end
+      context 'Andrew' do
+        let(:phrase) { 'Andrew' }
+        it { should eq('Andreway') }
+      end
+    end
+
+    context 'punctuation' do
+      let(:phrase) { 'Three things: one, two, three.' }
+      it { should eq('Eethray ingsthay: oneyay, otway, eethray.') }
     end
   end
 end
